76b79116d092e24853edb4b080fa13756b32918f
Ahmed Ali Ali <81569432+AhmedAliAli811@users.noreply.github.com>
2026-08-09 01:55:57 +0300

    Creating project structure and spliting the orignal excel file into multiple files based on the year-month

---
+++ README.md
@@ -0,0 +1,1 @@
+"# UCI_Retail_SSIS" 
---
+++ splitOrignalData.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+import os
+dataPath = f'D:\\Data Track\\Data Projects\\RetailETLSSIS\\orignal_data\\Online Retail.xlsx'
+dirPath = f'D:\\Data Track\\Data Projects\\RetailETLSSIS\\source_data\\'
+
+df = pd.read_excel(dataPath)
+
+def split_data_by_year_month(df):
+    df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
+    df['YearMonth'] = df['InvoiceDate'].dt.strftime('%Y-%m')
+
+    for year_month,group in df.groupby('YearMonth'):
+        output_path = os.path.join(dirPath, f'online_ratil_{year_month}.csv')
+        group.drop(columns = ['YearMonth']).to_csv(output_path)
+        print(f'Saved data for {year_month} to {output_path}')
+split_data_by_year_month(df)
