aefa201aac19f0afbc548adbac154233f874a29a
Ahmed Ali Ali <81569432+AhmedAliAli811@users.noreply.github.com>
2026-08-09 21:13:24 +0300

    Creating ETL audit and error logging tables

---
+++ sql/03_create_audit_error_tables.sql
@@ -0,0 +1,41 @@
+USE Retail_DW;
+go
+
+CREATE SCHEMA ETL;
+go
+
+
+
+CREATE TABLE ETL.AuditLog
+(
+    RunID INT IDENTITY(1,1) NOT NULL,
+    PackageName VARCHAR(100) NOT NULL,
+    StartTime DATETIME2 NOT NULL,
+    EndTime DATETIME2 NULL,
+    RowsRead INT NOT NULL DEFAULT 0,
+    RowsInserted INT NOT NULL DEFAULT 0,
+    RowsRejected INT NOT NULL DEFAULT 0,
+    Status VARCHAR(20) NOT NULL,
+    ErrorMessage NVARCHAR(1000) NULL,
+
+    CONSTRAINT PK_AuditLog PRIMARY KEY (RunID)
+);
+
+CREATE TABLE ETL.ErrorLog
+(
+    ErrorLogKey INT IDENTITY(1,1) NOT NULL,
+    RunID INT NOT NULL,
+    PackageName VARCHAR(100) NOT NULL,
+    ErrorDate DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+    InvoiceNo VARCHAR(25),
+    StockCode VARCHAR(25),
+    Description NVARCHAR(100),
+    Quantity INT,
+    UnitPrice DECIMAL(10,2),
+    CustomerID VARCHAR(20),
+    Country NVARCHAR(100),
+    ErrorReason VARCHAR(100) NOT NULL,
+
+    CONSTRAINT PK_ErrorLog PRIMARY KEY (ErrorLogKey),
+    CONSTRAINT FK_ErrorLog_AuditLog FOREIGN KEY (RunID) REFERENCES ETL.AuditLog(RunID)
+);
